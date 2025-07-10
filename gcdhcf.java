c306752e80aa2297dc1a1119f030052a3d8a0168
Urvashi Agrawal <158180920+urvashi-agrawal-dev@users.noreply.github.com>
2025-07-10 06:05:33 +0000

    fu

---
+++ gcdhcf.java
@@ -0,0 +1,17 @@
+class gcdhcf{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n1 = new Scanner(System.in);
+        int n2=new Scanner(System.in);
+        // int gcd=0;
+        for(int =1;i<=minimum(n1,n2);i++){
+            if(n1 % i==0 && n2%i==0){
+                // gcd=i;
+                System.out.print(i);
+                break;
+            }
+
+        }
+        System.out.print();
+    }
+}
